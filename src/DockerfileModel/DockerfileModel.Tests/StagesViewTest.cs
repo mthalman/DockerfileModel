26002cc7b2922f12d95b1c357caea922f9257f1a
Matt Thalman <mthalman@microsoft.com>
2020-09-17 06:11:50 -0500

    Stages view

---
+++ src/DockerfileModel/DockerfileModel.Tests/StagesViewTest.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace DockerfileModel.Tests
+{
+    public class StagesViewTest
+    {
+        [Fact]
+        public void SingleStage()
+        {
+            List<string> lines = new List<string>
+            {
+                "FROM image\n",
+                "ARG test=a\n",
+                "RUN echo 1"
+            };
+
+            Dockerfile dockerfile = Dockerfile.Parse(String.Join("", lines.ToArray()));
+
+            StagesView stagesView = new StagesView(dockerfile);
+            
+            Assert.Empty(stagesView.GlobalArgs);
+            
+            Assert.Single(stagesView.Stages);
+            Stage stage = stagesView.Stages.First();
+            Assert.Null(stage.Name);
+            Assert.Equal(lines[0], stage.FromInstruction.ToString());
+            Assert.Equal(2, stage.Lines.Count());
+            Assert.Equal(lines[1], stage.Lines.First().ToString());
+            Assert.Equal(lines[2], stage.Lines.Last().ToString());
+        }
+
+        [Fact]
+        public void SingleStageWithGlobalArgs()
+        {
+            List<string> lines = new List<string>
+            {
+                "ARG A\n",
+                "ARG B\n",
+                "#comment\n",
+                "\n",
+                "FROM image\n",
+                "#comment2\n",
+                "ARG test=a\n",
+                "RUN echo 1"
+            };
+
+            Dockerfile dockerfile = Dockerfile.Parse(String.Join("", lines.ToArray()));
+
+            StagesView stagesView = new StagesView(dockerfile);
+            
+            Assert.Equal(2, stagesView.GlobalArgs.Count());
+            Assert.Equal(lines[0], stagesView.GlobalArgs.First().ToString());
+            Assert.Equal(lines[1], stagesView.GlobalArgs.Last().ToString());
+            
+            Assert.Single(stagesView.Stages);
+            Stage stage = stagesView.Stages.First();
+            Assert.Null(stage.Name);
+            Assert.Equal(lines[4], stage.FromInstruction.ToString());
+            Assert.Equal(3, stage.Lines.Count());
+            Assert.Equal(lines[5], stage.Lines.First().ToString());
+            Assert.Equal(lines[6], stage.Lines.ElementAt(1).ToString());
+            Assert.Equal(lines[7], stage.Lines.Last().ToString());
+        }
+
+        [Fact]
+        public void MultiStage()
+        {
+            List<string> lines = new List<string>
+            {
+                "FROM image AS stage1\n",
+                "RUN echo 1\n",
+                "FROM image2 AS stage2\n",
+                "RUN echo 2\n",
+                "#comment\n",
+                "FROM image3 AS stage3\n",
+                "RUN echo 3"
+            };
+
+            Dockerfile dockerfile = Dockerfile.Parse(String.Join("", lines.ToArray()));
+
+            StagesView stagesView = new StagesView(dockerfile);
+
+            Assert.Empty(stagesView.GlobalArgs);
+
+            Assert.Equal(3, stagesView.Stages.Count());
+
+            Stage stage1 = stagesView.Stages.First();
+            Assert.Equal("stage1", stage1.Name);
+            Assert.Equal(lines[0], stage1.FromInstruction.ToString());
+            Assert.Single(stage1.Lines);
+            Assert.Equal(lines[1], stage1.Lines.First().ToString());
+
+            Stage stage2 = stagesView.Stages.ElementAt(1);
+            Assert.Equal("stage2", stage2.Name);
+            Assert.Equal(lines[2], stage2.FromInstruction.ToString());
+            Assert.Equal(2, stage2.Lines.Count());
+            Assert.Equal(lines[3], stage2.Lines.First().ToString());
+            Assert.Equal(lines[4], stage2.Lines.Last().ToString());
+
+            Stage stage3 = stagesView.Stages.Last();
+            Assert.Equal("stage3", stage3.Name);
+            Assert.Equal(lines[5], stage3.FromInstruction.ToString());
+            Assert.Single(stage3.Lines);
+            Assert.Equal(lines[6], stage3.Lines.First().ToString());
+        }
+
+        [Fact]
+        public void MultiStageWithGlobalArgs()
+        {
+            List<string> lines = new List<string>
+            {
+                "ARG ARG1\n",
+                "FROM image AS stage1\n",
+                "RUN echo 1\n",
+                "FROM image2 AS stage2\n",
+                "RUN echo 2\n",
+                "#comment\n",
+                "FROM image3 AS stage3"
+            };
+
+            Dockerfile dockerfile = Dockerfile.Parse(String.Join("", lines.ToArray()));
+
+            StagesView stagesView = new StagesView(dockerfile);
+
+            Assert.Single(stagesView.GlobalArgs);
+            Assert.Equal(lines[0], stagesView.GlobalArgs.First().ToString());
+
+            Assert.Equal(3, stagesView.Stages.Count());
+
+            Stage stage1 = stagesView.Stages.First();
+            Assert.Equal("stage1", stage1.Name);
+            Assert.Equal(lines[1], stage1.FromInstruction.ToString());
+            Assert.Single(stage1.Lines);
+            Assert.Equal(lines[2], stage1.Lines.First().ToString());
+
+            Stage stage2 = stagesView.Stages.ElementAt(1);
+            Assert.Equal("stage2", stage2.Name);
+            Assert.Equal(lines[3], stage2.FromInstruction.ToString());
+            Assert.Equal(2, stage2.Lines.Count());
+            Assert.Equal(lines[4], stage2.Lines.First().ToString());
+            Assert.Equal(lines[5], stage2.Lines.Last().ToString());
+
+            Stage stage3 = stagesView.Stages.Last();
+            Assert.Equal("stage3", stage3.Name);
+            Assert.Equal(lines[6], stage3.FromInstruction.ToString());
+            Assert.Empty(stage3.Lines);
+        }
+    }
+}
---
+++ src/DockerfileModel/DockerfileModel/StagesView.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Validation;
+
+namespace DockerfileModel
+{
+    public class StagesView
+    {
+        public StagesView(Dockerfile dockerfile)
+        {
+            Requires.NotNull(dockerfile, nameof(dockerfile));
+
+            List<DockerfileLine> lines = dockerfile.Lines.ToList();
+
+            List<ArgInstruction> globalArgs = new List<ArgInstruction>();
+            List<Stage> stages = new List<Stage>();
+            FromInstruction? currentStage = null;
+            List<DockerfileLine> stageLines = new List<DockerfileLine>();
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                DockerfileLine line = lines[i];
+                if (currentStage is null)
+                {
+                    if (line is ParserDirective || line is Whitespace)
+                    {
+                        continue;
+                    }
+                    else if (line is ArgInstruction argInstruction)
+                    {
+                        globalArgs.Add(argInstruction);
+                    }
+                    else if (line is FromInstruction fromInstruction)
+                    {
+                        currentStage = fromInstruction;
+                    }
+                }
+                else
+                {
+                    if (line is FromInstruction nextFromInstruction)
+                    {
+                        stages.Add(new Stage(currentStage, stageLines));
+                        currentStage = nextFromInstruction;
+                        stageLines = new List<DockerfileLine>();
+                    }
+                    else
+                    {
+                        stageLines.Add(line);
+                    }
+                }
+            }
+
+            if (currentStage != null)
+            {
+                stages.Add(new Stage(currentStage, stageLines));
+            }
+
+            GlobalArgs = globalArgs;
+            Stages = stages;
+        }
+
+        public IEnumerable<ArgInstruction> GlobalArgs { get; }
+        public IEnumerable<Stage> Stages { get; }
+    }
+
+    public class Stage
+    {
+        public Stage(FromInstruction fromInstruction, IEnumerable<DockerfileLine> lines)
+        {
+            FromInstruction = fromInstruction;
+            Lines = lines;
+        }
+
+        public FromInstruction FromInstruction { get; }
+        public string? Name => FromInstruction.StageName;
+        public IEnumerable<DockerfileLine> Lines { get; }
+    }
+}
