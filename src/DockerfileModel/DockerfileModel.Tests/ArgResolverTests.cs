6d50b016b0eb3fb9e2b085d68c364fc6f1b5fd86
Matt Thalman <mthalman@microsoft.com>
2020-09-18 05:59:24 -0500

    Arg resolver (#12)

--- src/DockerfileModel/DockerfileModel.Tests/ArgResolverTests.cs
+++ src/DockerfileModel/DockerfileModel.Tests/ArgResolverTests.cs
@@ -3,18 +3,19 @@ using Xunit;
 
 namespace DockerfileModel.Tests
 {
-    public class InstructionBaseTests
+    public class ArgResolverTests
     {
         [Theory]
         [InlineData("alpine:prefix-$TAG", '\\', "TAG", "test", null, null, "alpine:prefix-test")]
-        [InlineData("alpine:$TAGx", '\\', "TAG", "test", null, null, "alpine:$TAGx")]
+        [InlineData("alpine:$TAGx", '\\', "TAG", "test", null, null, "alpine:")]
         [InlineData("alpine:$TAG-x", '\\', "TAG", "test", null, null, "alpine:test-x")]
         [InlineData("alpine:$TAG-suffix", '\\', "TAG", "test", null, null, "alpine:test-suffix")]
         [InlineData("$image", '\\', "image", "test", null, null, "test")]
         [InlineData("$image:$tag", '\\', "image", "test", "tag", "foo", "test:foo")]
         [InlineData("alpine:`$TAG", '`', "TAG", "test", null, null, "alpine:`$TAG")]
-        public void ResolveArgValues(
-            string imageName, char escapeChar, string arg1Name, string arg1Value, string arg2Name, string arg2Value, string expectedImageName)
+        [InlineData("$image$tag", '`', "image", "test", "tag", ":foo", "test:foo")]
+        public void Resolve(
+            string text, char escapeChar, string arg1Name, string arg1Value, string arg2Name, string arg2Value, string expected)
         {
             Dictionary<string, string> args = new Dictionary<string, string>
             {
@@ -25,9 +26,8 @@ namespace DockerfileModel.Tests
                 args.Add(arg2Name, arg2Value);
             }
 
-            FromInstruction line = FromInstruction.Create(imageName);
-            line.ResolveArgValues(args, escapeChar);
-            Assert.Equal(expectedImageName, line.ImageName);
+            string actual = ArgResolver.Resolve(text, args, escapeChar);
+            Assert.Equal(expected, actual);
         }
     }
 }
