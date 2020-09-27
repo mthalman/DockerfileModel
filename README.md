0fdfa514c27eb9f641e6452f35d8c63ddacf476b
Matt Thalman <mthalman@microsoft.com>
2020-09-27 16:14:55 -0500

    Add scenario tests (#25)

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Dockerfile Model Library
 
-This .NET library provides a structured model of the Dockerfile syntax for the purposes of parsing and generating Dockerfiles.
+This .NET library provides a structured model of the Dockerfile syntax for the purposes of parsing and generating Dockerfiles. It provides full fidelity of the file contents meaning that the parsed content can be output back to a string and produce the same content character-for-character, including whitespace. This makes it ideal for parsing as well as workflows that require programmatically modifying existing Dockerfiles.
+
+## Usage
+
+The library is available as a NuGet package: [DockerfileModel](https://www.nuget.org/packages/DockerfileModel/).
+
+For code examples, check out the [scenario tests](https://github.com/mthalman/DockerfileModel/blob/main/src/DockerfileModel/DockerfileModel.Tests/ScenarioTests.cs) which demonstrate how the API can be used for various scenarios.
---
+++ src/DockerfileModel/DockerfileModel.Tests/ScenarioTests.cs
@@ -0,0 +1,230 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DockerfileModel.Tokens;
+using Xunit;
+
+namespace DockerfileModel.Tests
+{
+    public class ScenarioTests
+    {
+        /// <summary>
+        /// The structure of a Dockerfile consists of instructions, whitespace, comments, and parser directives.
+        /// </summary>
+        [Fact]
+        public void DockerfileStructureAndConstructTypes()
+        {
+            string dockerfileContent = TestHelper.ConcatLines(new List<string>
+            {
+                "# escape=`",
+                "FROM scratch",
+                "    ",
+                "# TODO"
+            });
+
+            // Parse the Dockerfile
+            Dockerfile dockerfile = Dockerfile.Parse(dockerfileContent);
+
+            // Verify its structure
+
+            Assert.Equal('`', dockerfile.EscapeChar);
+
+            DockerfileConstruct[] constructs = dockerfile.Items.ToArray();
+
+            Assert.Equal(4, constructs.Length);
+            
+            Assert.Equal(ConstructType.ParserDirective, constructs[0].Type);
+            Assert.IsType<ParserDirective>(constructs[0]);
+
+            Assert.Equal(ConstructType.Instruction, constructs[1].Type);
+            Assert.IsType<FromInstruction>(constructs[1]);
+
+            Assert.Equal(ConstructType.Whitespace, constructs[2].Type);
+            Assert.IsType<Whitespace>(constructs[2]);
+
+            Assert.Equal(ConstructType.Comment, constructs[3].Type);
+            Assert.IsType<Comment>(constructs[3]);
+        }
+
+        /// <summary>
+        /// Change the tag of an image name referenced in a FROM instruction.
+        /// </summary>
+        [Fact]
+        public void ChangeTag()
+        {
+            string dockerfileContent = TestHelper.ConcatLines(new List<string>
+            {
+                "FROM alpine:3.11",
+                "RUN echo \"Hello World\""
+            });
+
+            // Parse the Dockerfile
+            Dockerfile dockerfile = Dockerfile.Parse(dockerfileContent);
+            FromInstruction fromInstruction = dockerfile.Items.OfType<FromInstruction>().First();
+
+            // Parse the image name into its component parts
+            ImageName imageName = ImageName.Parse(fromInstruction.ImageName);
+
+            // Change the tag value and set the image name with the new value
+            imageName.Tag = "3.12";
+            fromInstruction.ImageName = imageName.ToString();
+
+            // Verify the new tag value is output
+            string expectedOutput = TestHelper.ConcatLines(new List<string>
+            {
+                "FROM alpine:3.12",
+                "RUN echo \"Hello World\""
+            });
+            Assert.Equal(expectedOutput, dockerfile.ToString());
+        }
+
+        /// <summary>
+        /// Resolves both overriden and default ARG values that are referenced throughout a Dockerfile.
+        /// </summary>
+        [Fact]
+        public void ResolveArguments_Globally()
+        {
+            string dockerfileContent = TestHelper.ConcatLines(new List<string>
+            {
+                "FROM alpine",
+                "ARG FIRST_NAME=Thomas",
+                "ARG LAST_NAME=Edison",
+                "RUN echo \"Hello, $FIRST_NAME $LAST_NAME\""
+            });
+
+            // Parse the Dockerfile
+            Dockerfile dockerfile = Dockerfile.Parse(dockerfileContent);
+
+            // Resolve reference arg values, overriding FIRST_NAME.
+            // This modifies the underlying values of the model, replacing any references to
+            // arguments with their resolved values. Be aware of this if your intention is
+            // write the model back to the Dockerfile on disk.
+            dockerfile.ResolveArgValues(new Dictionary<string, string>
+            {
+                { "FIRST_NAME", "Tom" }
+            });
+
+            // Verify the arg values have been resolved
+            string expectedOutput = TestHelper.ConcatLines(new List<string>
+            {
+                "FROM alpine",
+                "ARG FIRST_NAME=Thomas",
+                "ARG LAST_NAME=Edison",
+                "RUN echo \"Hello, Tom Edison\""
+            });
+            Assert.Equal(expectedOutput, dockerfile.ToString());
+        }
+
+        /// <summary>
+        /// Resolves ARG values that are referenced from an instruction without modifying the underlying model.
+        /// </summary>
+        [Fact]
+        public void ResolveArguments_FromValue()
+        {
+            string dockerfileContent = TestHelper.ConcatLines(new List<string>
+            {
+                "ARG REPO=alpine",
+                "ARG TAG=latest",
+                "FROM $REPO:$TAG"
+            });
+
+            // Parse the Dockerfile
+            Dockerfile dockerfile = Dockerfile.Parse(dockerfileContent);
+            FromInstruction fromInstruction = dockerfile.Items.OfType<FromInstruction>().First();
+
+            // Resolve arg values on the ImageName and have the resolved value returned
+            string resolvedImageName = dockerfile.ResolveArgValues(fromInstruction, inst => inst.ImageName);
+
+            // Verify the image name has the args resolved
+            Assert.Equal("alpine:latest", resolvedImageName);
+            
+            // Verify the underlying value has the arg references maintained
+            string expectedOutput = TestHelper.ConcatLines(new List<string>
+            {
+                "ARG REPO=alpine",
+                "ARG TAG=latest",
+                "FROM $REPO:$TAG"
+            });
+            Assert.Equal(expectedOutput, dockerfile.ToString());
+        }
+
+        /// <summary>
+        /// Each construct within a Dockerfile is made up of tokens of various types. Some tokens
+        /// are aggregate tokens that contain other more primitive tokens.
+        /// </summary>
+        [Fact]
+        public void Tokens()
+        {
+            string dockerfileContent = TestHelper.ConcatLines(new List<string>
+            {
+                "ARG TAG=latest",
+                "FROM alpine:$TAG \\",
+                "  AS build"
+            });
+
+            // Parse the Dockerfile
+            Dockerfile dockerfile = Dockerfile.Parse(dockerfileContent);
+
+            DockerfileConstruct[] dockerfileConstructs = dockerfile.Items.ToArray();
+            
+            // Verify the individual tokens that are contained in the ARG instruction
+            Token[] repoArgTokens = dockerfileConstructs[0].Tokens.ToArray();
+            Assert.Equal(6, repoArgTokens.Length);
+            Assert.IsType<KeywordToken>(repoArgTokens[0]);
+            Assert.IsType<WhitespaceToken>(repoArgTokens[1]);
+            Assert.IsType<IdentifierToken>(repoArgTokens[2]);
+            Assert.IsType<SymbolToken>(repoArgTokens[3]);
+            Assert.IsType<LiteralToken>(repoArgTokens[4]);
+            Assert.IsType<NewLineToken>(repoArgTokens[5]);
+
+            // Verify the individual tokens that are contained in the FROM instruction
+            Token[] fromInstructionTokens = dockerfileConstructs[1].Tokens.ToArray();
+            Assert.Equal(7, fromInstructionTokens.Length);
+            Assert.IsType<KeywordToken>(fromInstructionTokens[0]);
+            Assert.IsType<WhitespaceToken>(fromInstructionTokens[1]);
+            Assert.IsType<LiteralToken>(fromInstructionTokens[2]);
+            Assert.IsType<WhitespaceToken>(fromInstructionTokens[3]);
+            Assert.IsType<LineContinuationToken>(fromInstructionTokens[4]);
+            Assert.IsType<NewLineToken>(fromInstructionTokens[5]);
+
+            // StageName is an aggregate token that contains other tokens
+            Assert.IsType<StageName>(fromInstructionTokens[6]);
+            StageName stageName = (StageName)fromInstructionTokens[6];
+            Token[] stageNameTokens = stageName.Tokens.ToArray();
+            Assert.Equal(4, stageNameTokens.Length);
+            Assert.IsType<WhitespaceToken>(stageNameTokens[0]);
+            Assert.IsType<KeywordToken>(stageNameTokens[1]);
+            Assert.IsType<WhitespaceToken>(stageNameTokens[2]);
+            Assert.IsType<IdentifierToken>(stageNameTokens[3]);
+        }
+
+        /// <summary>
+        /// Comments can either be top-level or nested within an multi-line instruction.
+        /// </summary>
+        [Fact]
+        public void InstructionsWithComments()
+        {
+            string dockerfileContent = TestHelper.ConcatLines(new List<string>
+            {
+                "#top-level comment",
+                "FROM alpine \\",
+                "  #nested comment",
+                "  AS build",
+                "#top-level comment",
+            });
+
+            // Parse the Dockerfile
+            Dockerfile dockerfile = Dockerfile.Parse(dockerfileContent);
+
+            DockerfileConstruct[] dockerfileConstructs = dockerfile.Items.ToArray();
+            Assert.Equal(3, dockerfileConstructs.Length);
+
+            Assert.IsType<Comment>(dockerfileConstructs[0]);
+            
+            Assert.IsType<FromInstruction>(dockerfileConstructs[1]);
+            FromInstruction fromInstruction = (FromInstruction)dockerfileConstructs[1];
+            Assert.Single(fromInstruction.Comments);
+
+            Assert.IsType<Comment>(dockerfileConstructs[2]);
+        }
+    }
+}
