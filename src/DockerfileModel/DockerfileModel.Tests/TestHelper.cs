a13747da00553fbd1aa55bf7862a55d4fd3cc994
Matt Thalman <mthalman@microsoft.com>
2020-09-27 16:12:52 -0500

    Add scenario tests

---
+++ src/DockerfileModel/DockerfileModel.Tests/TestHelper.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DockerfileModel.Tests
+{
+    public static class TestHelper
+    {
+        public static string ConcatLines(IEnumerable<string> lines, string lineEnding = "\n") =>
+            string.Join(lineEnding, lines.ToArray());
+    }
+}
